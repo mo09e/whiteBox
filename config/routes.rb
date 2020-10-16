021f789930b69a23a299850c956d141ad516f235
Moe <moe_akahane@diveintocode.jp>
2020-10-16 16:28:46 +0900

    Add:Userコントローラ、ビューを生成。ルーティング追加 #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :homes, only: [:index]
   root to: "homes#index"
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "sign_in", to: "users/sessions#new"
+    get "sign_out", to: "users/sessions#destroy"
+  end
 end
