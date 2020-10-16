9ae8c297c10a684c66005ed8f1c7e05154488439
Moe <moe_akahane@diveintocode.jp>
2020-10-17 01:34:22 +0900

    Add:Artist情報登録・編集機能を実装 #5

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,30 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+  before_action :current_user, only: [:edit, :destroy, :update, :new]
+
   def index
+    @artists = Artist.all
   end
 
   def new
+    if params[:back]
+      @artist = Artist.new(artist_params)
+    else
+      @artist = Artist.new
+    end
+  end
+
+  def create
+    @artist = current_user.build_artist(artist_params)
+    if params[:back]
+      render :new
+    else
+      if @artist.save
+        redirect_to artists_path, notice: "作家情報を登録しました"
+      else
+        render :new
+      end
+    end
   end
 
   def show
@@ -11,6 +33,20 @@ class ArtistsController < ApplicationController
   def edit
   end
 
-  def _form
+  def update
+    if @artist.update(artist_params)
+      redirect_to artist_path(@artist), notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :note, :phone_number, { images: []}, :url)
+                                   .merge(user_id: current_user.id)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ApplicationRecord
   belongs_to :user
+  mount_uploaders :images, ArtistImageUploader
 end
