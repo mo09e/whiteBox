d7669cb41c2bcb86d5b7de2e462be1eb7d91b7de
Moe <moe_akahane@diveintocode.jp>
2020-10-29 17:14:25 +0900

    Fix:スタイルを変更 #29

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,6 @@ CarrierWave.configure do |config|
   require 'carrierwave/storage/abstract'
   require 'carrierwave/storage/file'
   require 'carrierwave/storage/fog'
-  # config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
